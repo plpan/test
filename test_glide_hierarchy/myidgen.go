04e163596e7ad3a03f379d99a84737da84ed2393
panpeilong <panpeilong@didichuxing.com>
2017-11-24 16:34:35 +0800

    update glide_hierarchy with lower version

--- test_glide_hierarchy/myidgen.go
+++ test_glide_hierarchy/myidgen.go
@@ -2,9 +2,12 @@ package test_glide_hierarchy
 
 import (
 	"github.com/sumory/idgen"
+	"github.com/fatih/color"
 )
 
 func NextId() (error, int64) {
+	color.Blue("Prints %s in blue.", "text")
+
 	_, idWorker := idgen.NewIdWorker(1)
 	return idWorker.NextId()
 }
