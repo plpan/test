9b04982c528c7f4700ab1d0346a213f5c09b235f
panpeilong <panpeilong@didichuxing.com>
2017-11-24 17:55:56 +0800

    test glide with version conflict, glide does not resolve version conflict

--- test_glide/main.go
+++ test_glide/main.go
@@ -4,8 +4,12 @@ import (
     "fmt"
     //"github.com/sumory/idgen"
 	"github.com/plpan/test/test_glide_hierarchy"
+	"github.com/fatih/color"
 )
 
 func main() {
 	fmt.Println(test_glide_hierarchy.NextId())
+
+	c := color.New(color.FgCyan).Add(color.Underline)
+	c.Println("Prints cyan text with an underline.")
 }
