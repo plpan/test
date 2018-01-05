feb160ff86f2f909f0ad675f756284673811b861
pplonepiece@126.com <pplonepiece@126.com>
2018-01-05 11:22:00 +0800

    test receiver with replacement

--- test_go/test_receiver.go
+++ test_go/test_receiver.go
@@ -1,25 +1,51 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 type User struct {
-    Name string
+	Name string
 }
 
-func (u *User) Echo() {
-    fmt.Println(u.Name)
-    u.Name = "hahah"
+// receiver是对象，则是临时对象
+func (u User) EchoObject() {
+	u.Name = "ahaha"
+	fmt.Printf("receiver: %p name: %s\n", &u, u.Name)
 }
 
-/*func (u User) Echo() {
-    fmt.Println(u.Name)
-    u.Name = "ahaha"
-}*/
+// receiver是指针，则是引用
+func (u *User) EchoPointer() {
+	u.Name = "hahah"
+	fmt.Printf("receiver: %p name: %s\n", u, u.Name)
+}
+
+// 对receiver的替换不会起作用
+func (u *User) Rename() {
+	user := User{
+		"stupig",
+	}
+	fmt.Printf("receiver: %p temp: %p name: %s\n", u, &user, u.Name)
+	// assign to receiver directly
+	// do not work: assignment to method receiver propagates only to callees but not to callers
+	u = &user
+	fmt.Printf("receiver: %p temp: %p name: %s\n", u, &user, u.Name)
+}
 
 func main() {
-    u := User{Name: "onepiece"}
-    u.Echo()
-    fmt.Println(u.Name)
+	u := User{Name: "onepiece"}
+	fmt.Println("---echo object---")
+	fmt.Printf("receiver: %p  name: %s\n", &u, u.Name)
+	u.EchoObject()
+	fmt.Printf("receiver: %p  name: %s\n", &u, u.Name)
+
+	fmt.Println("---echo pointer---")
+	fmt.Printf("receiver: %p  name: %s\n", &u, u.Name)
+	u.EchoPointer()
+	fmt.Printf("receiver: %p  name: %s\n", &u, u.Name)
+
+	fmt.Println("---rename---")
+	fmt.Printf("receiver: %p  name: %s\n", &u, u.Name)
+	u.Rename()
+	fmt.Printf("receiver: %p  name: %s\n", &u, u.Name)
 }
