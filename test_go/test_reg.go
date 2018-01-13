df1187dc355853e76061e8c23d61c7de23a6513b
pplonepiece@126.com <pplonepiece@126.com>
2018-01-13 17:58:30 +0800

    test regexp matchstring:

--- test_go/test_reg.go
+++ test_go/test_reg.go
@@ -7,4 +7,6 @@ import (
 
 func main() {
 	fmt.Println(regexp.MustCompile("\\\033\\[[0-9]+m").Split("\033[32mdogo version 0.0.3 installed successfully \033[0m", 3)[1])
+	// 这个MatchString我真是没话说了
+	fmt.Println(regexp.MustCompile("^[A-Za-z0-9]*$").MatchString("Aaaa-a"))
 }
