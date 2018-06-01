a0be8ef915be931abbf4b746b98400e2dfeb3866
pplonepiece <pplonepiece@126.com>
2018-06-01 17:04:01 +0800

    update gorequest with showing open fds

--- test_go/test_gorequest.go
+++ test_go/test_gorequest.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
 	"sync"
 	"time"
 
@@ -11,12 +15,26 @@ import (
 var wg sync.WaitGroup
 
 func testRequest() {
-	r := gorequest.New().Timeout(time.Millisecond * 1000).Get("http://www.baidu.com")
+	r := gorequest.New()
+	r.Transport.DisableKeepAlives = true
+	r.Timeout(time.Millisecond * 1000).Get("http://www.baidu.com")
 	_, _, err := r.End()
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	fmt.Println("Request done, open files =", countOpenFiles())
+
 	wg.Done()
+}
+
+func countOpenFiles() int {
+	out, err := exec.Command("/bin/sh", "-c", fmt.Sprintf("lsof -p %v", os.Getpid())).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(string(out), "\n")
+	return len(lines) - 1
 }
 
 func main() {
