1d5d85cfc9ab50ae54bedb3dd46e0897d7e897f5
pplonepiece <pplonepiece@126.com>
2018-07-15 00:34:52 +0800

    test with asm

---
+++ test_asm/test_const.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var const_id int // readonly
+
+func main() {
+	fmt.Println(const_id)
+}
---
+++ test_asm/test_const.s
@@ -0,0 +1,4 @@
+#include "textflag.h"
+
+GLOBL ·const_id(SB),NOPTR|RODATA,$8
+DATA  ·const_id+0(SB)/8,$9527
